51ff938856471c541239acbb927023b8a3d7f292
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-05-12 15:33:38 -0400

    cleaned out old file

--- optimizeGeometry.java
+++
@@ -1,9 +0,0 @@
-/*****************************************************************************
- * Author: David Fridovich-Keil
- * Course: ELE 583
- *
- * This file does two things.
- * (1) It checks if all constraints are simultaneously satisfiable.
- * (2) If possible, it executes all constraints in the proper sequence so as
- *     to ensure that they are properly satisfied.
- ****************************************************************************/
